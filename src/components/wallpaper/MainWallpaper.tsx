b755f04bc5127dc1820d096b0bc145cdcc0eb980
rifatNR <Rifatnoor92@gmail.com>
2024-08-20 03:16:08 +0600

    base wallpaper working

---
+++ src/components/wallpaper/MainWallpaper.tsx
@@ -0,0 +1,15 @@
+const MainWallpaper = () => {
+    return (
+        <div className="h-screen w-screen bg-orange-600 text-black font-extrabold flex items-center justify-center">
+            <div>
+                <div className="flex items-end mb-[-6rem] space-x-5">
+                    <div className="text-[7rem] mb-[-10px]">H!</div>
+                    <div className="text-[3rem]">I&apos;m</div>
+                </div>
+                <div className="text-[15rem]">RIFAT</div>
+            </div>
+        </div>
+    );
+};
+
+export default MainWallpaper;
